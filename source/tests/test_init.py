76156d1686f3003462800051d6c6d04cb93a58fd
xammi-1 <kmg.xammi.1@gmail.com>
2015-03-14 01:18:33 +0300

    lstrip problem is solved

--- source/tests/test_init.py
+++ source/tests/test_init.py
@@ -9,10 +9,16 @@ from lib import fix_market_url
 class InitTestCase(unittest.TestCase):
     def test_fix_market_url(self):
         lstrip = Mock(return_value='test')
-        with patch('string.lstrip', lstrip):
-            fix_market_url('')
 
-        lstrip.assert_called_once_with("market://")
-        assert_calls_amount(lstrip, 1)
+        result = fix_market_url('market://_my_app')
+        assert result == "http://play.google.com/store/apps/_my_app"
+
+        result = fix_market_url('market://my_app')
+        assert result == "http://play.google.com/store/apps/y_app"
+
+        result = fix_market_url('market://market')
+        assert result == "http://play.google.com/store/apps/"
+
+        # lstrip should be replaced with re.sub(url, re.compile('^market://'))
 
 
