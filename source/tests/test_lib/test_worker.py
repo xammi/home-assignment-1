1fb98b8b89e8343b9aee03b60aea9ac173601cd3
xammi-1 <kmg.xammi.1@gmail.com>
2015-03-15 22:14:33 +0300

    Test for redirect_history

--- source/tests/test_lib/test_worker.py
+++ source/tests/test_lib/test_worker.py
@@ -8,16 +8,31 @@ DEFAULT_URL = 'http://myurl.com:8080/folder/file.exe'
 
 
 class Task:
-    def __init__(self, task_id):
+    def __init__(self, task_id, data):
         self.task_id = task_id
+        self.data = data
 
 
 class WorkerTestCase(unittest.TestCase):
-    def test_get_redirect_history_from_task(self):
-        task = Task(1)
-        task.data = {'url': DEFAULT_URL, 'not_recheck': 'not_recheck', 'url_id': 2}
-        final_data = {'url': DEFAULT_URL, 'not_recheck': 'not_recheck', 'url_id': 2, 'recheck': True}
 
-        with patch('lib.worker.get_redirect_history', return_value=(['ERROR'], [], [])):
+    def test_get_redirect_history_from_task_ok(self):
+        redirect_history = [[], [], []]
+        init_data = {'url': DEFAULT_URL, 'url_id': 2}
+        final_data = {'url_id': 2, 'result': redirect_history, 'check_type': 'normal'}
+
+        task = Task(1, init_data)
+        with patch('lib.worker.get_redirect_history', return_value=redirect_history):
             result = get_redirect_history_from_task(task, 1)
+
+        self.assertEqual(result, (False, final_data))
+
+    def test_get_redirect_history_from_task_error(self):
+        redirect_history = [['ERROR'], [], []]
+        init_data = {'url': DEFAULT_URL, 'url_id': 2}
+        final_data = {'url': DEFAULT_URL, 'url_id': 2, 'recheck': True}
+
+        task = Task(1, init_data)
+        with patch('lib.worker.get_redirect_history', return_value=redirect_history):
+            result = get_redirect_history_from_task(task, 1)
+
         self.assertEqual(result, (True, final_data))
