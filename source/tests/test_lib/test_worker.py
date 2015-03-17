89561321269d48dd60bca6c69dddca09b2578ae9
xammi-1 <kmg.xammi.1@gmail.com>
2015-03-17 19:39:33 +0300

    Extra test for worker

--- source/tests/test_lib/test_worker.py
+++ source/tests/test_lib/test_worker.py
@@ -36,6 +36,25 @@ class WorkerTestCase(unittest.TestCase):
 
         self.assertEqual(result, (False, final_data))
 
+    def test_get_redirect_history_from_task_with_recheck(self):
+        redirect_history = [[], ['http://ya.ru'], []]
+        init_data = {
+            'url': DEFAULT_URL,
+            'url_id': URL_ID,
+            'recheck': True
+        }
+        final_data = {
+            'url_id': URL_ID,
+            'result': redirect_history,
+            'check_type': 'normal'
+        }
+
+        task = Task(1, init_data)
+        with patch('lib.worker.get_redirect_history', return_value=redirect_history):
+            result = get_redirect_history_from_task(task, 1)
+
+        self.assertEqual(result, (False, final_data))
+
     def test_get_redirect_history_from_task_error(self):
         redirect_history = [['ERROR'], [], []]
         init_data = {
